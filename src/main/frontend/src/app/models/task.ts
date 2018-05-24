086bed24ada0b7ee082b808182502ff7ed30f94b
Mr Modise <modisemorebodi@gmail.com>
2018-05-24 11:30:23 +0200

    updated the task model

--- src/main/frontend/src/app/models/task.ts
+++ src/main/frontend/src/app/models/task.ts
@@ -3,16 +3,14 @@ import {Optional} from "@angular/core";
 export interface ITask {
     id?: number,
     taskName: string,
-    dueDate: string,
     completed: boolean
 }
 
 export class Task implements ITask {
-    dueDate: string;
-
-    constructor(public taskName, public completed, @Optional() public id?) {
+    constructor(public taskName, public completed, public dueDate, @Optional() public id?) {
         this.taskName = taskName;
         this.id = id;
+        this.dueDate = dueDate;
         this.completed = completed;
     }
 }
